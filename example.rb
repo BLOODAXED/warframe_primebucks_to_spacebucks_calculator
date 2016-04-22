b8561cad091d36d953291ca2c7e8d963720484df
bloodaxed <bloodaxed@gmail.com>
2016-04-22 18:52:46 -0400

    made array of just the prices. fixed best_ten

--- example.rb
+++ example.rb
@@ -19,8 +19,9 @@ end
 
 def getHighBuyAndLowSell(array_of_buy, array_of_sell)    
     best_five = Array.new
-    best_five.push(array_of_buy.values_at(0...5))
-    best_five.push(array_of_sell[0...5])
+    array_of_buy[0...5].each{ |a| best_five.push(a) }
+    array_of_sell[0...5].each{ |b| best_five.push(b) }
+    return best_five
 end
 
 def makeFile(file_name, contents)
@@ -28,20 +29,30 @@ def makeFile(file_name, contents)
     somefile.puts contents
     somefile.close
 end
+def justThePrices(input)
+    prices = Array.new
+    input.each{ |a| prices.push( a["price"] ) }
+    return prices
+end
 
-files           = ["sorted_sell.txt", "sorted_buy.txt", "best_ten.txt"]
+files           = ["sorted_sell.txt", "sorted_buy.txt", "best_ten.txt", "prices.txt"]
 request_as_json = getUrlAsJson("http://warframe.market/api/get_orders/Set/Akbronco%20Prime%20Set")
 sorted_sell     = getSortedSellOrders(request_as_json)
 sorted_buy      = getSortedSellOrders(request_as_json)
 best_ten        = getHighBuyAndLowSell(sorted_buy, sorted_sell)
+prices          = justThePrices(best_ten)
 
 makeFile(files[0], sorted_sell)
 makeFile(files[1], sorted_buy)
 makeFile(files[2], best_ten)
+makeFile(files[3], prices)
 
-puts "sell"
-puts sorted_sell
-puts "buy"
-puts sorted_buy
-puts "best 10"
-puts best_ten
+puts "create files:"
+puts files
+
+# puts "sell"
+# puts sorted_sell
+# puts "buy"
+# puts sorted_buy
+# puts "best 10"
+# puts best_ten
