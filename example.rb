b78f5c51e7edb95e275ff0434a6dd634da82da47
james7 <jbarron711@gmail.com>
2016-04-23 11:17:11 -0500

    Added code to calculate average and standard deviation.  Added code to remove outliers that are some number of standard deviation away from the average.

--- example.rb
+++ example.rb
@@ -31,16 +31,44 @@ def makeFile(file_name, contents)
     somefile.puts contents
     somefile.close
 end
+
 def justThePrices(input)
     prices = Array.new
     input.each{ |a| prices.push( a["price"] ) }
     return prices
 end
 
+### find the average and standard deviation of array_of_prices["price"] and remove any entries that are
+### greater than or less than average +- num_delta * standard deviation.
+def removeOutliers(array_of_prices,num_delta)
+    std_deviation_of_prices = calculateStandardDevation(array_of_prices)
+    average_of_prices = calculateAverage(array_of_prices)
+    avg_puls_2_delta = (average_of_prices + num_delta*std_deviation_of_prices)
+    avg_mins_2_delta = (average_of_prices - num_delta*std_deviation_of_prices)
+    return array_of_prices.reject{|a| a["price"] > avg_puls_2_delta or a["price"] < avg_mins_2_delta}
+end
+
+### returns the average of all array_of_prices["price"] values
+def calculateAverage(array_of_prices)
+    temp_sum = array_of_prices.inject(0.0){ |sum, a| sum + a["price"]}
+    average = temp_sum / (array_of_prices.size)
+    return average
+end
+
+### returns the standard devision of all array_of_prices["price"] values
+def calculateStandardDevation(array_of_prices)
+    average = calculateAverage(array_of_prices)
+    deviation = array_of_prices.inject(0.0){ |sum, a| sum + ((a["price"]-average)**2)}
+    std_deviation = Math.sqrt(deviation / (array_of_prices.size - 1))
+    return std_deviation
+end
+
 files           = ["sorted_sell.txt", "sorted_buy.txt", "best_ten.txt", "prices.txt"]
 request_as_json = getUrlAsJson("http://warframe.market/api/get_orders/Set/Akbronco%20Prime%20Set")
 sorted_sell     = getSortedSellOrders(request_as_json)
+sorted_sell     = removeOutliers(sorted_sell,1.5)
 sorted_buy      = getSortedBuyOrders(request_as_json)
+sorted_buy      = removeOutliers(sorted_buy,1.5)
 best_ten        = getHighBuyAndLowSell(sorted_buy, sorted_sell)
 prices          = justThePrices(best_ten)
 
