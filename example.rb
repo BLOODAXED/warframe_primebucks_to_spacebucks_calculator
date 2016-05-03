6ea99959f9a5fcb223e3f367a192f09b5d6ebaf7
bloodaxed <bloodaxed@gmail.com>
2016-05-03 16:22:26 -0400

    created a function that takes the ducat table from the wiki and makes a csv out of it

--- example.rb
+++ example.rb
@@ -2,21 +2,19 @@ require "http"
 require "json"
 
 def getUrlAsJson(url)
-    requested_set_response = HTTP.get(url).to_s
-    request_json = JSON.parse(requested_set_response)
-    return request_json
+	requested_set_response = HTTP.get(url).to_s
+	request_json = JSON.parse(requested_set_response)
+	return request_json
 end
 
-### Returns the sell orders with the smallest price in the first index
 def getSortedSellOrders(json)
-    sorted_sells = json["response"]["sell"].sort_by { |i| i["price"]}
-    return sorted_sells
+	sorted_sells = json["response"]["sell"].sort_by { |i| i["price"]}
+	return sorted_sells
 end
 
-### Returns buy orders with the largest price in the first index.
 def getSortedBuyOrders(json)
     sorted_buys = json["response"]["buy"].sort_by { |i| i["price"]}
-    return sorted_buys.reverse
+    return sorted_buys
 end
 
 def getHighBuyAndLowSell(array_of_buy, array_of_sell)
@@ -31,44 +29,16 @@ def makeFile(file_name, contents)
     somefile.puts contents
     somefile.close
 end
-
 def justThePrices(input)
     prices = Array.new
     input.each{ |a| prices.push( a["price"] ) }
     return prices
 end
 
-### find the average and standard deviation of array_of_prices["price"] and remove any entries that are
-### greater than or less than average +- num_delta * standard deviation.
-def removeOutliers(array_of_prices,num_delta)
-    std_deviation_of_prices = calculateStandardDevation(array_of_prices)
-    average_of_prices = calculateAverage(array_of_prices)
-    avg_puls_2_delta = (average_of_prices + num_delta*std_deviation_of_prices)
-    avg_mins_2_delta = (average_of_prices - num_delta*std_deviation_of_prices)
-    return array_of_prices.reject{|a| a["price"] > avg_puls_2_delta or a["price"] < avg_mins_2_delta}
-end
-
-### returns the average of all array_of_prices["price"] values
-def calculateAverage(array_of_prices)
-    temp_sum = array_of_prices.inject(0.0){ |sum, a| sum + a["price"]}
-    average = temp_sum / (array_of_prices.size)
-    return average
-end
-
-### returns the standard devision of all array_of_prices["price"] values
-def calculateStandardDevation(array_of_prices)
-    average = calculateAverage(array_of_prices)
-    deviation = array_of_prices.inject(0.0){ |sum, a| sum + ((a["price"]-average)**2)}
-    std_deviation = Math.sqrt(deviation / (array_of_prices.size - 1))
-    return std_deviation
-end
-
 files           = ["sorted_sell.txt", "sorted_buy.txt", "best_ten.txt", "prices.txt"]
 request_as_json = getUrlAsJson("http://warframe.market/api/get_orders/Set/Akbronco%20Prime%20Set")
 sorted_sell     = getSortedSellOrders(request_as_json)
-sorted_sell     = removeOutliers(sorted_sell,1.5)
-sorted_buy      = getSortedBuyOrders(request_as_json)
-sorted_buy      = removeOutliers(sorted_buy,1.5)
+sorted_buy      = getSortedSellOrders(request_as_json)
 best_ten        = getHighBuyAndLowSell(sorted_buy, sorted_sell)
 prices          = justThePrices(best_ten)
 
